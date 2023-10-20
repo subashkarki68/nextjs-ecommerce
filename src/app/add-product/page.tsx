1276c192bb11144d9de9041f0b98e69c2b806a26
subash karki <subashkarki68@gmail.com>
2023-10-20 18:42:22 +0545

    Add authentication to add-product page

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -1,6 +1,8 @@
 import { prisma } from "@/lib/db/prisma";
 import { redirect } from "next/navigation";
 import FormSubmitButton from "@/components/FormSubmitButton";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 
 export const metadata = {
   title: "Add Product - Flomazon",
@@ -8,6 +10,8 @@ export const metadata = {
 
 async function addProduct(formData: FormData) {
   "use server";
+  const session = await getServerSession(authOptions);
+  if (!session) redirect("/api/auth/signin?callbackUrl=/add-product");
   const name = formData.get("name")?.toString();
   const description = formData.get("description")?.toString();
   const imageUrl = formData.get("imageUrl")?.toString();
@@ -23,7 +27,9 @@ async function addProduct(formData: FormData) {
   redirect("/");
 }
 
-export default function AddProductPage() {
+export default async function AddProductPage() {
+  const session = await getServerSession(authOptions);
+  if (!session) redirect("/api/auth/signin?callbackUrl=/add-product");
   return (
     <div>
       <h1 className="text-lg mb-3 font-bold">Add Product</h1>
